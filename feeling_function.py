241499fbfdd415fc5fa0ea069bfea2d11c4a679c
DevAnderson95 <DevAnderson95@gmail.com>
2025-02-04 06:33:24 -0500

    First Commit

---
+++ feeling_function.py
@@ -0,0 +1,32 @@
+import math
+counter = []
+def count_up(review):
+    
+    return len(review)
+review = input("Please enter your review: ") 
+positive_words = ["good", "excellent", "great", "awesome", "fantastic", "superb", "amazing"]
+negative_words = ["bad", "poor", "terrible", "horrible", "awful", "disappointing", "subpar"]
+
+word_count1 = positive_words
+word_count2 = negative_words
+
+for words in review:
+    if words.lower() in word_count1:
+        (len(words))
+    elif words.lower() in word_count2:
+        print(len(words))
+    else:
+        print(review)
+    
+    
+
+
+print(count_up(review))
+
+
+
+    
+
+    
+        
+    
---
+++ product_reviews.py
@@ -0,0 +1,34 @@
+reviews = [
+        "This product is really good. I'm impressed with its quality.\n",
+        "The performance of this product is excellent. Highly recommended!\n",
+        "I had a bad experience with this product. It didn't meet my expectations.\n",
+        "Poor quality product. Wouldn't recommend it to anyone.\n",
+        "The product was average. Nothing extraordinary about it.\n"
+]
+
+keys = ['good', 'bad', 'excellent', 'poor', 'average']
+def product_review(reviews,keys):
+        capped_words = []
+        for review in reviews: 
+            for key in keys:
+                review = review.replace(key, key.upper()) 
+                capped_words.append(review)
+        return capped_words
+
+         
+new_review = product_review(reviews, keys)
+
+
+for review in new_review:
+    print(review)
+
+        
+        
+    
+    
+    
+
+    
+
+
+        
